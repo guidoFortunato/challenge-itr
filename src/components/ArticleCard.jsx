6cd7d00d8f41b2d6cb63add84c2a06134e4a09d9
Guido <46787485+guidoFortunato@users.noreply.github.com>
2022-09-02 15:32:41 -0300

    cambios

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,17 +1,29 @@
 import { Tags } from "./Tags";
 
 export const ArticleCard = ({ data }) => {
+  const dataTaxonomy = data.taxonomy.tags;
+
+  // const [dataTaxonomy, setDataTaxonomy] = useState(data.taxonomy.tags);
+
+  // if (dataTaxonomy.length === 0) {
+  //   console.log("cero");
+  // }
   return (
-    <div className="col-12 col-md-6 col-lg-4 col-xl-3">
-      <div className="card">
-        {/* <img src="..." className="card-img-top" alt="img" /> */}
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            <Tags tagsList={data.taxonomy} />
-          </p>
+    <>
+    {/* elimino los que no tienen tags */}
+      {dataTaxonomy.length > 0 && (
+        <div className="col-12 col-md-6 col-lg-4 col-xl-3">
+          <div className="card">
+            {/* <img src="..." className="card-img-top" alt="img" /> */}
+            <div className="card-body">
+              <h5 className="card-title">Tags</h5>
+              <p className="card-text">
+                <Tags tagsList={data.taxonomy} />
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
+
 export const Tags = ({ tagsList }) => {
   const { tags } = tagsList;
+  // console.log(tagsList)
+  // console.log(tags);
 
   return (
     <>
-      {tags.map(( item, index) => (
-        <span key={index} >{ item.slug } - { item.text }</span>
-      ))}
-      
+      {tags.map((item, index) => {
+        // console.log(item);
+        
+          return (
+            <span key={index}>
+              {item.slug} <br />
+            </span>
+          );
+       
+      })}
     </>
   );
 };
