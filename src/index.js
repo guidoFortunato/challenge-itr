75c62a5942498861cfe558aa9dabc3ecce359b57
Guido <46787485+guidoFortunato@users.noreply.github.com>
2022-09-06 02:11:51 -0300

    agrego hashrouter

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { DataProvider } from "./context/DataProvider";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AppRouter } from "./routers/AppRouter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <DataProvider>
         <AppRouter />
       </DataProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
